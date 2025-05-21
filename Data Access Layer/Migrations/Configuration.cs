4b2968e6ae282b93f4978f47826d62d7420518bc
Rayquzz <cristianghel20041@gmail.com>
2025-05-21 15:40:59 +0300

    added new location on contact page

---
+++ Data Access Layer/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace Data_Access_Layer.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Data_Access_Layer.AppDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Data_Access_Layer.AppDbContext";
+        }
+
+        protected override void Seed(Data_Access_Layer.AppDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
