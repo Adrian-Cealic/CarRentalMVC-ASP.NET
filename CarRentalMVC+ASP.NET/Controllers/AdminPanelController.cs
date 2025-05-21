12d626f36e4ae078901f1ddbd79c1babaa13b672
Rayquzz <cristianghel20041@gmail.com>
2025-05-21 21:49:23 +0300

    added new controller for AdminPanel

---
+++ CarRentalMVC+ASP.NET/Controllers/AdminPanelController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarRentalMVC_ASP.NET.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        private readonly YourDbContext db = new YourDbContext(); // înlocuiește cu contextul tău
+
+        // GET: AdminPanel
+        public ActionResult Index()
+        {
+            var users = db.Users.ToList();
+            return View(users);
+        }
+
+        // Exemplu pentru editare user
+        public ActionResult Edit(int id)
+        {
+            var user = db.Users.Find(id);
+            if (user == null)
+                return HttpNotFound();
+            return View(user);
+        }
+
+        // Adaugă și alte acțiuni pentru administrare după nevoie
+    }
+}
