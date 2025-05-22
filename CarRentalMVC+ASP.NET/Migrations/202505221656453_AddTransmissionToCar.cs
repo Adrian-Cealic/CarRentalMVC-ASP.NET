9e5b2a3dbce978dcd1cfa28e6096475a0e09e00c
MistaJuice1 <leon.revencu@gmail.com>
2025-05-22 19:57:42 +0300

    Add migration for transmission feature in car management
    - Added new migration files to include transmission details in the car database schema.
    - Updated project file to compile new migration classes and resources.

---
+++ CarRentalMVC+ASP.NET/Migrations/202505221656453_AddTransmissionToCar.cs
@@ -0,0 +1,18 @@
+﻿namespace CarRentalMVC_ASP.NET.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddTransmissionToCar : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Cars", "Transmission", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Cars", "Transmission");
+        }
+    }
+}
